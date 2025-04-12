573a5923ae999a470954f7194439d831f9965260
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2025-04-13 00:32:32 +0800

    fix: fix profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,13 @@
 'use client';
+import React from 'react'
 
 import { FaArrowLeft, FaHome, FaUserCircle, FaCalendarAlt } from 'react-icons/fa';
 import { useRouter } from 'next/navigation'; // Import useRouter
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
